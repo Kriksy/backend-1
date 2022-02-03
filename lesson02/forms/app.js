d0a80b533b6a378d2cc4d2862538e79a20ffccb7
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-03 13:43:46 +0100

    Update form demo

--- lesson02/forms/app.js
+++ lesson02/forms/app.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const app = express()
 const PORT = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "html", "form.html"))
 });
 
 app.get("/submit", (req, res) => {
-  const {name, age, color} = req.query;
-  res.send(`
+  res.send(renderResponse(req.query));
+});
+
+app.post("/submit", (req, res) => {
+  res.send(renderResponse(req.body));
+});
+
+const renderResponse = ({name, age, color}) => `
     <h1>Hello ${name}!</h1>
     <p>You're ${age} years old</p>
     <p>Your favorite color is ${color}</p>
-  `)
-});
+  `;
 
 app.listen(PORT, () => {
   console.log(`Started Express server on port ${PORT}`);
