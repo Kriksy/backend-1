94ea158561c78c50126218a49b28515331c934bd
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-03 12:33:29 +0100

    Add static files example

---
+++ lesson02/express-static/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express()
+const PORT = 3000;
+const STATIC_ROOT = path.join(__dirname, "static");
+
+app.get("/", (_req, res) => {
+  res.sendFile("./index.html", {root: STATIC_ROOT});
+})
+
+app.get("/:fileName", (req, res) => {
+  res.sendFile(req.params.fileName, {root: STATIC_ROOT});
+})
+
+app.listen(PORT, () => {
+  console.log(`Started Express server on port ${PORT}`);
+});
