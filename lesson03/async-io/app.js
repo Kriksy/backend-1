19f7935659b2b0269341d3e734bfe8cf084b91c6
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-04 16:13:35 +0100

    Add example of async I/O

---
+++ lesson03/async-io/app.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.readFile(path.join(__dirname, "hello.txt"), "utf8", (_error, data) => {
+  console.log("Finished reading the file:", data);
+});
+console.log("This gets printed first");
+
+// Note that an error in a callback doesn't crash the program:
+fs.readFile(path.join(__dirname, "foobar.txt"), "utf8", (error, _data) => {
+  console.log("Error callback:", error);
+});
+
+// This will crash the program, however:
+// throw "Crash!";
+// console.log("This will not get printed");
