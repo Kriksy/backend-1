82fa933f6cdf63169e3b8a2751c78f16b0f4b5e9
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-04 16:14:03 +0100

    Fix readfile example

--- lesson01/readfiles/readfile.js
+++ lesson01/readfiles/readfile.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const filepath = path.join(__dirname, "/testfile.txt");
+const filepath = path.join(__dirname, "testfile.txt");
 
 fs.readFile(filepath, "utf8", (_err, data) => {
   console.log(data);
