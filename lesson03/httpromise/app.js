a651c1e759b803c7064dd0e7651d1b723167bd75
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-05 01:42:09 +0100

    Update promises

--- lesson03/httpromise/app.js
+++ lesson03/httpromise/app.js
@@ -16,6 +16,7 @@ const getPromise = (options) => {
   });
 };
 
+/*
 getPromise({
   hostname: "icanhazdadjoke.com",
   port: 443,
@@ -23,3 +24,36 @@ getPromise({
   method: "GET",
   headers: {"Accept": "text/plain"}
 }).then((data) => { console.log(data) });
+
+
+const time = Date.now();
+const p1 = getPromise({
+  hostname: "icanhazdadjoke.com",
+  port: 443,
+  path: "/",
+  method: "GET",
+  headers: {"Accept": "text/plain"}
+}).then((data) => {
+  return Date.now()
+});
+const p2 = getPromise({
+  hostname: "twitter.com",
+  port: 443,
+  path: "/",
+  method: "GET",
+  headers: {"Accept": "text/plain"}
+}).then((data) => {
+  return Date.now()
+});
+Promise.all([p1, p2]).then(([v1, v2]) => console.log(v1, v2));
+
+const p3 = getPromise({
+  hostname: "twitter.com",
+  port: 443,
+  path: "/",
+  method: "GET",
+  headers: {"Accept": "text/plain"}
+}).then((data) => {
+  return data;
+}).then((data) => console.log(data));
+*/
