e1783fbf7f6144af5eb5cd4abf558fc24bd4b1fd
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-08 22:14:46 +0100

    Add multer example

---
+++ lesson02/multer/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const app = express()
+const PORT = 3000;
+
+
+const upload = multer({ dest: 'uploads/' })
+
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "upload.html"))
+});
+
+// NB: We can also upload single files using upload.single()
+
+app.post(
+  "/upload",
+  upload.array("files"),
+  (req, res) => {
+    console.log(req.body);
+    console.log(req.files);
+    res.send("Files uploaded successfully")
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Started Express server on port ${PORT}`);
+});
