1d6f8cb9d98c8561be525518ce0ab39a2a62bec9
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-09 14:23:39 +0100

    Update file upload example

--- live/lesson02/file-upload/app.js
+++ live/lesson02/file-upload/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
+
+const upload = multer({ dest: "uploads" });
+
+app.use(upload.single("file"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.post(
+    "/upload",
+    (req, res) => {
+        res.send("Files uploaded successfully!");
+    }
+);
 
 app.listen(PORT, () => {
     console.log(`Started server on port: ${PORT}`)
