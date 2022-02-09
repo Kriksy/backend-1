71a4c2f2cd25f4597dcb989de64b7c0df89a3e4b
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-09 15:07:11 +0100

    Update EJS example

--- live/lesson02/ejs/app.js
+++ live/lesson02/ejs/app.js
@@ -4,7 +4,18 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World! 🚀")
+    const data = {
+        title: "Lord of the Rings",
+        heroes: [
+            {name: "Frodo", origin: "The shire"},
+            {name: "Samwise", origin: "The shire"},
+            {name: "Merry", origin: "The shire"},
+            {name: "Pippin", origin: "The shire"},
+            {name: "Boromir", origin: "Gondor"},
+            {name: "Gandalf"}
+        ]
+    }
+    res.render("index.ejs", data)
 })
 
 app.listen(PORT, () => {
