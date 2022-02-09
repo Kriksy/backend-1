3787a7d241e4058f19c1e644e44e0cfab84635ec
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-09 15:31:48 +0100

    Update EJS example

--- live/lesson02/ejs/app.js
+++ live/lesson02/ejs/app.js
@@ -3,19 +3,27 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const HEROES = [
+    {name: "Frodo", origin: "The shire"},
+    {name: "Samwise", origin: "The shire"},
+    {name: "Merry", origin: "The shire"},
+    {name: "Pippin", origin: "The shire"},
+    {name: "Boromir", origin: "Gondor"},
+    {name: "Gandalf"}
+]
+
 app.get("/", (req, res) => {
     const data = {
         title: "Lord of the Rings",
-        heroes: [
-            {name: "Frodo", origin: "The shire"},
-            {name: "Samwise", origin: "The shire"},
-            {name: "Merry", origin: "The shire"},
-            {name: "Pippin", origin: "The shire"},
-            {name: "Boromir", origin: "Gondor"},
-            {name: "Gandalf"}
-        ]
+        heroes: HEROES
     }
     res.render("index.ejs", data)
+})
+
+app.get("/:heroName", (req, res) => {
+    const heroName = req.params.heroName;
+    const hero = HEROES.filter(({ name }) => name === heroName)[0];
+    res.render("hero.ejs", hero);
 })
 
 app.listen(PORT, () => {
