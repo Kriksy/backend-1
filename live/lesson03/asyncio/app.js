0398972e8bf13c6645b85f8ba22d43777d7f646f
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-14 13:21:42 +0100

    Update async example

--- live/lesson03/asyncio/app.js
+++ live/lesson03/asyncio/app.js
@@ -1,6 +1,20 @@
 fs = require("fs");
 
+// Asynchronous error handling
 fs.readFile("hello.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.error("Error while reading file", err);
+        return;
+    }
     console.log(data);
 });
+
+// Synchronous error handling
+const foo = {bar: {baz: "foobar"}};
+try {
+    console.log(foo.baz.bar);
+} catch (error) {
+    console.error("Something went wrong!");
+}
+
 console.log("Hej!");
