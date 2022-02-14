75df50a15daa519ba099eb3a849a0dc151ef142e
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-14 10:29:04 +0100

    Update books example

--- live/lesson02/books/app.js
+++ live/lesson02/books/app.js
@@ -5,6 +5,20 @@ const { BOOKS } = require("./books");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.render("books.ejs", {books: BOOKS});
+});
+
+app.get("/books/:bookId", (req, res) => {
+    const bookId = parseInt(req.params.bookId);
+    const book = BOOKS.find((book) => book.id === bookId);
+    if (book) {
+        res.render("book.ejs", book);
+    } else {
+        res.status(404).send(`<h1>Not found</h1>`);
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Started server on port: ${PORT}`)
 });
