40bbb439d3e389eabf1c8cbd0481310629db2fac
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-15 22:45:43 +0100

    Updat Express boilerplate

--- express-boilerplate/app.js
+++ express-boilerplate/app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express()
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
 app.listen(PORT, () => {
   console.log(`Started Express server on port ${PORT}`);
 });
