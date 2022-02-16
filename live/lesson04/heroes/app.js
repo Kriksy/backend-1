0f03c586dd8d5ec19b280682e060beae312d9da0
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-16 13:47:49 +0100

    Add Heroes example

---
+++ live/lesson04/heroes/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+// const mongoose = require("mongoose");
+
+const app = express()
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(PORT, () => {
+  console.log(`Started Express server on port ${PORT}`);
+});
