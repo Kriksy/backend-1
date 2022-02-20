4f0d083f07f3aee7846308bf24233eb51b6e16fe
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-20 23:51:56 +0100

    Use MongoDB for session storage

--- lesson05/passport/app.js
+++ lesson05/passport/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const authRouter = require("./auth").router;
 const diaryRouter = require("./diary").router;
@@ -9,12 +10,14 @@ const diaryRouter = require("./diary").router;
 
 const app = express()
 const PORT = 3000;
+const MONGO_URL = "mongodb://127.0.0.1/backend1";
 
 app.use(express.urlencoded({extended: true}));
 app.use(session({
   secret: "keyboard cat",
   resave: false,
   saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: MONGO_URL })
 }));
 app.use(passport.authenticate('session'));
 
@@ -22,7 +25,7 @@ app.use("/auth", authRouter);
 app.use("/", diaryRouter);
 
 
-mongoose.connect("mongodb://127.0.0.1/backend1");
+mongoose.connect(MONGO_URL);
 app.listen(PORT, () => {
   console.log(`Started Express server on port ${PORT}`);
 });
