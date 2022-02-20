4eb902032828869a8886fd80a915a971accbb17e
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-20 23:43:19 +0100

    Update to use MongoDB exclusively

--- lesson05/passport/auth.js
+++ lesson05/passport/auth.js
@@ -1,38 +1,16 @@
 const express = require("express");
 const passport = require("passport");
-const LocalStrategy = require("passport-local");
 
 const router = express.Router();
 
 const { User } = require("./models/user");
-
-// const users = {"bella": "twilight"}
-
-// passport.use(new LocalStrategy((username, password, callback) => {
-//   if (users[username] === password) {
-//     callback(null, username);
-//   } else {
-//     callback(null, false);
-//   }
-// }));
-
-
-// // Save user to session. NB: This can also be asynchronous!
-// passport.serializeUser((user, callback) => {
-//   return callback(null, user);
-// });
-
-// // Load user from session. NB: This can also be asynchronous!
-// passport.deserializeUser((user, callback) => {
-//     return callback(null, user);
-// });
 
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-router.get("/signup", (req, res) => {
+router.get("/signup", (_req, res) => {
   res.render("signup.ejs");
 });
 
--- lesson05/passport/diary.js
+++ lesson05/passport/diary.js
@@ -1,29 +1,22 @@
 const express = require("express");
 const { ensureLoggedIn } = require('connect-ensure-login');
 
+const { DiaryEntry } = require("./models/diary");
+
 const router = express.Router();
-
-
-const entries = {
-  "bella": [
-    {
-      title: "Lunch with Edward",
-      content: `Today I had lunch with Edward. 
-        He's such a nice guy I don't even mind he's a vampire`,
-      date: new Date(2022, 1, 15, 12, 14, 0)
-    }
-  ]
-};
 
 router.use(ensureLoggedIn("/auth/login"));
 
-router.get("/", (req, res) => {
-  res.render("index.ejs", {entries: entries[req.user]});
+router.get("/", async (req, res) => {
+  const entries = await DiaryEntry.find({user: req.user._id});
+  res.render("index.ejs", {entries});
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const {title, content} = req.body;
-  entries[req.user].push({title, content, date: new Date()});
+  const user = req.user;
+  const entry = new DiaryEntry({title, content, user: user._id});
+  await entry.save();
   res.redirect("/");
 });
 
