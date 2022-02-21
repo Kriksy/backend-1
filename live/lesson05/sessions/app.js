c6a0e5936bd1b1c1a913b0e5a4c9c7e5265e171f
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-21 14:34:59 +0100

    Update cookie example

--- live/lesson05/sessions/app.js
+++ live/lesson05/sessions/app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const crypto = require("crypto");
 
 const app = express()
 const PORT = 3000;
+
+app.use(cookieParser());
+app.use(express.urlencoded());
 
 const USERS = {
   "chreke": "secret"
 };
 
 const SESSIONS = {};
-
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   const sessionKey = req.cookies.session;
@@ -21,6 +23,19 @@ app.get("/", (req, res) => {
     res.sendStatus(401);
   }
 });
+
+app.post("/login", (req, res) => {
+  const {username, password} = req.body;
+  if (USERS[username] === password) {
+    const sessionId = crypto.randomBytes(64).toString('hex');
+    SESSIONS[sessionId] = username;
+    console.log("sessions:", SESSIONS);
+    res.cookie("session", sessionId, {maxAge: 30 * 1000});
+    res.redirect("/");
+  } else {
+
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Started Express server on port ${PORT}`);
