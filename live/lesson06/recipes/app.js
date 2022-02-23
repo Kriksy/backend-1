54e9c2e54c0334c05dbe265cc84050c0e1668a71
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-23 15:49:32 +0100

    Update recipes example

--- live/lesson06/recipes/app.js
+++ live/lesson06/recipes/app.js
@@ -9,6 +9,27 @@ const JWT_SECRET = "0823uoiwehfilusayciadfsbhkjsdfbjaenrw";
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    const authHeader = req.header("Authorization");
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+        req.user = jwt.verify(token, JWT_SECRET);
+    }
+    next();
+});
+
+const requireLogin = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+app.get("/secret", requireLogin, (req, res) => {
+    res.json({message: `Hello ${req.user.username}`});
+});
+
 app.post("/tokens", async (req, res) => {
     const {username, password} = req.body;
     const user = await User.login(username, password);
@@ -17,7 +38,7 @@ app.post("/tokens", async (req, res) => {
         const token = jwt.sign(
             {userId, username: user.username},
             JWT_SECRET,
-            {expiresIn: 120, subject: userId}
+            {expiresIn: "1h", subject: userId}
         );
         res.json({token});
     } else {
