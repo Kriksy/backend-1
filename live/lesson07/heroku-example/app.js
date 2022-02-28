2c760f03eb4092b483567cd718195250d829de76
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-28 13:04:33 +0100

    Update Heroku example

--- live/lesson07/heroku-example/app.js
+++ live/lesson07/heroku-example/app.js
@@ -6,14 +6,16 @@ const mongoose = require("mongoose");
 const { DiaryEntry } = require("./models/diary");
 
 const app = express();
+
 const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 app.get("/", async (req, res) => {
     const entries = await DiaryEntry.find();
     res.render("index.ejs", {entries});
 });
 
-mongoose.connect("mongodb://localhost/backend1");
+mongoose.connect(MONGODB_URI);
 
 app.listen(PORT, () => {
   console.log(`Started Express server on port ${PORT}`);
