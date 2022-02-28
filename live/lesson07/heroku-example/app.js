729ccaa0f66570bc3934ab7e3ce87045386f320e
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-02-28 13:17:35 +0100

    Update Heroku example

--- live/lesson07/heroku-example/app.js
+++ live/lesson07/heroku-example/app.js
@@ -10,9 +10,18 @@ const app = express();
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", async (req, res) => {
     const entries = await DiaryEntry.find();
     res.render("index.ejs", {entries});
+});
+
+app.post("/entries", async (req, res) => {
+    const {title, content} = req.body;
+    const entry = new DiaryEntry({title, content});
+    await entry.save();
+    res.redirect("/");
 });
 
 mongoose.connect(MONGODB_URI);
