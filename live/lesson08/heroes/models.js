83018957d96637b055fbb466ec5613072aad6069
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-03-02 10:25:14 +0100

    Add Heroes example

---
+++ live/lesson08/heroes/models.js
@@ -0,0 +1,15 @@
+const { seq } = require("async");
+const { DataTypes, Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("mysql://root:superhemligt@localhost:3306/lotr");
+
+const Hero = sequelize.define({
+    name: {
+        type: DataTypes.STRING, 
+        allowNull: false, 
+        unique: true
+    },
+    age: { type: DataTypes.INTEGER, validate: { min: 0 }}
+});
+
+module.exports = {sequelize, Hero};
