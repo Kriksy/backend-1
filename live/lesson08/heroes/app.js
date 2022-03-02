eae4deb5ea11df4d8ae57d08f3189a49dc474f56
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-03-02 10:46:03 +0100

    Add support for creating heroes

--- live/lesson08/heroes/app.js
+++ live/lesson08/heroes/app.js
@@ -4,9 +4,18 @@ const { Hero, sequelize } = require("./models");
 const app = express()
 const PORT = 3000;
 
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", async (req, res) => {
     const heroes = await Hero.findAll();
     res.render("index.ejs", { heroes });
+});
+
+app.post("/heroes", async (req, res) => {
+    const { name, age } = req.body;
+    const hero = Hero.build({ name, age });
+    await hero.save();
+    res.redirect("/");
 });
 
 sequelize.sync({alter: true}).then(() => {
